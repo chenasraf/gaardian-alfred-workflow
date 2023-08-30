ded8ebda13be395906efc2d106afa6058463090c
Chen Asraf <casraf@pm.me>
2023-08-30 16:34:08 +0300

    feat: add fallback search on google

--- map.mjs
+++ map.mjs
@@ -5,10 +5,16 @@ function getMaps(name) {
 }
 
 const results = getMaps(process.argv[2])
-
-console.log(JSON.stringify({
-  items: results.map((it) => ({
-    title: `Open Gaardian Map for "${it.name}"`,
-    arg: it.href,
-  }))
+const items = results.map((it) => ({
+  title: `Open Gaardian Map for "${it.name}"`,
+  arg: it.href,
 }))
+
+// if (!items.length) {
+items.push({
+  title: `Search for "aardwolf ${process.argv[2]} map" on Google`,
+  arg: `https://www.google.com/search?q=aardwolf+${process.argv[2]}+map`,
+})
+// }
+
+console.log(JSON.stringify({ items }))
